6674804114272a6a467e3f5ab9a5af8d501723ac
HotShot0901 <contact.hot.shor.1234@gmail.com>
2025-09-26 15:25:28 +0530

    refactor: Move logger initialization to constructor in EchelonEditor

--- EchelonEditor/EchelonEditor.cpp
+++ EchelonEditor/EchelonEditor.cpp
@@ -2,21 +2,26 @@
 
 class EchelonEditor : public Echelon::Application {
 public:
-    void Run() override {
-        Echelon::Logger logger("EchelonEditor");
-        logger.AddSink(Echelon::ConsoleSink);
-        logger.AddSink(Echelon::FileSink("EchelonEditor.log"));
-
-        logger.Info("Echelon Editor Started");
-        for (int i = 0; i < 5; ++i) {
-            logger.Trace("Trace message " + std::to_string(i));
-            logger.Debug("Debug message " + std::to_string(i));
-            logger.Info("Info message " + std::to_string(i));
-            logger.Warn("Warning message " + std::to_string(i));
-            logger.Error("Error message " + std::to_string(i));
-        }
-        logger.Fatal("Echelon Editor Ending");
+    EchelonEditor() : m_Logger("EchelonEditor") {
+        m_Logger.AddSink(Echelon::ConsoleSink);
+        m_Logger.AddSink(Echelon::FileSink("EchelonEditor.log"));
     }
+    ~EchelonEditor() override = default;
+
+    void Run() override {
+        m_Logger.Info("Echelon Editor Started");
+        for (int i = 0; i < 5; ++i) {
+            m_Logger.Trace("Trace message " + std::to_string(i));
+            m_Logger.Debug("Debug message " + std::to_string(i));
+            m_Logger.Info("Info message " + std::to_string(i));
+            m_Logger.Warn("Warning message " + std::to_string(i));
+            m_Logger.Error("Error message " + std::to_string(i));
+        }
+        m_Logger.Fatal("Echelon Editor Ending");
+    }
+
+private:
+    Echelon::Logger m_Logger;
 };
 
 Echelon::Application* CreateApplication() {
